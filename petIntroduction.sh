1262766b696fa259059bb3acd1fb7b903227dc28
armaneousuf <armaneousuf97@gmail.com>
2026-04-08 00:40:58 +0600

    improved: improved by adding :(colon)

--- petIntroduction.sh
+++ petIntroduction.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-read -r -p "What's your pet's name " pet_name
-read -r -p "What type of animal it is? " type_of_animal
-read -r -p "Can you tell me it's age " age
+read -r -p "What's your pet's name: " pet_name
+read -r -p "What type of animal it is?: " type_of_animal
+read -r -p "Can you tell me it's age: " age
 
 sleep 2
 
-read -r -p "Do you want to see the pet's profile " profile
+read -r -p "Do you want to see the pet's profile: " profile
 
 if [ "$profile" = "yes" ]; then 
   echo "Your $type_of_animal named $pet_name is $age years old!"
