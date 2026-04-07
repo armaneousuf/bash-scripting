f35c621297065cfc5ad694d1cd868240d249a369
armaneousuf <armaneousuf97@gmail.com>
2026-04-07 19:34:38 +0600

    feat: used -r

--- lesson2.sh
+++ lesson2.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 
 echo "What's your name?"
-read name
+read -r name
 sleep 1
 
 echo "Hello, $name."
 
 echo "How old are you?"
-read age
+read -r age
 sleep 1
 
 echo "Does that mean you're $age years old?"
 
 echo "Do you want your summary? (yes/no)"
-read answer 
+read -r answer 
 sleep 1
 
 if [ "$answer" = "yes" ]; then
