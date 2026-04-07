a7e72a6291d259bce86f1dc8b382f45d65a93b2d
armaneousuf <armaneousuf97@gmail.com>
2026-04-08 01:00:20 +0600

    improved: not much

---
+++ practice1.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash 
+read -r -p "What's your name: " name
+#echo "what's your name: " 
+#read -r name
+
+read -r -p "What's your age: " age
+#echo "what's your age: "
+#read -r age
+
+echo "Hello, $name. You are $age years old"
