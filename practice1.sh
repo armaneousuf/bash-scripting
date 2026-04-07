614c039405b50d8485fe4db7e3228348a020b8d7
armaneousuf <armaneousuf97@gmail.com>
2026-04-08 01:40:21 +0600

    feat: added new variable

--- practice1.sh
+++ practice1.sh
@@ -8,3 +8,18 @@ read -r -p "What's your age: " age
 #read -r age
 
 echo "Hello, $name. You are $age years old"
+sleep 2
+
+echo "Calculating"
+echo ".........."
+sleep 1
+echo "**........"
+sleep 1
+echo "****......"
+sleep 1
+echo "******...."
+sleep 1
+echo "**********"
+
+random_age=$((( RANDOM % 19 ) + age ))
+echo "$name, you might be alive at $random_age"
