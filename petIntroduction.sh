b7644379493483b3401b6c1376ae72d4fa49ffcc
armaneousuf <armaneousuf97@gmail.com>
2026-04-08 00:48:34 +0600

    improve: added or operators to catch other possible fishes

--- petIntroduction.sh
+++ petIntroduction.sh
@@ -8,7 +8,7 @@ sleep 2
 
 read -r -p "Do you want to see the pet's profile: " profile
 
-if [ "$profile" = "yes" ]; then 
+if [ "$profile" = "yes" ] || [ "$profile" = "y" ] || [ "$profile" = "YES" ] || [ "$profile" = "Y" ]; then 
   echo "Your $type_of_animal named $pet_name is $age years old!"
 else 
   echo "Not everyone likes to pet animals"
