4d0cd6d4f7d531d77f13b2f27cc71adf25974c31
armaneousuf <armaneousuf97@gmail.com>
2026-04-08 00:37:33 +0600

    feat: added petIntroduction.sh by applying the same formula of lesson2.sh

---
+++ petIntroduction.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+read -r -p "What's your pet's name " pet_name
+read -r -p "What type of animal it is? " type_of_animal
+read -r -p "Can you tell me it's age " age
+
+sleep 2
+
+read -r -p "Do you want to see the pet's profile " profile
+
+if [ "$profile" = "yes" ]; then 
+  echo "Your $type_of_animal named $pet_name is $age years old!"
+else 
+  echo "Not everyone likes to pet animals"
+fi
