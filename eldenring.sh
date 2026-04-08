637c9c6919d54acec421e63244dfb9bd61aad0a4
armaneousuf <armaneousuf97@gmail.com>
2026-04-08 17:00:01 +0600

    feat: added new bash

---
+++ eldenring.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+#First beast battle
+
+beast=$(( RANDOM % 2 ))
+echo "Your first beast approaches. Prepare to battle. Pick a number between 0-1 (0/1)"
+
+read -r tarnished
+
+if [[ "$beast" == "$tarnished" ]]; then
+    echo "Beast VANQUISHED!! Congrats fellow tarnished"
+else 
+    echo "You Died!"
+  exit 1
+fi
+
+
+sleep 2
+echo "Boss battle. Get scared. It's Margit. Pick a number between 0-9. (0-9)"
+
+read -r tarnished
+
+beast=$((RANDOM % 10))
+if [[ "$beast" == "$tarnished" ]]; then
+  echo "Beast VANQUISHED"
+else 
+  echo "You Died"
+fi
